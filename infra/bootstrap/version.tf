bdb95d22275b3ab864dd80560a1a9b92dfd089d8
zoolaph <94008067+zoolaph@users.noreply.github.com>
2025-08-10 09:32:10 +0200

    Fixing the accidental .terraform push that had me use the git filter-repo that wibes the remote config so origin vanished (#4)

---
+++ infra/bootstrap/version.tf
@@ -0,0 +1,15 @@
+terraform {
+
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.60"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
